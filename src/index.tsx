a4f1df79e91d45c63fa97a2af9c1315e73916109
TaigaShono <taigashono0612@gmail.com>
2025-03-24 05:43:06 +0900

    deleted .tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // 'react-dom' ではなく 'react-dom/client' を使用
-import App from './App.tsx';
+import App from './App';
 
 // React アプリケーションのルート作成
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
